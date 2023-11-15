4f60eeb2a1701c4a230a2dd83e0333c5bbc0a123
Shahmir-Muhammad <147588981+Shahmir-Muhammad@users.noreply.github.com>
2023-11-15 10:25:39 -0800

    Add files via upload

---
+++ LAB TASK 7 BY SHAHMIR.cpp
@@ -0,0 +1,65 @@
+/*#include <iostream>
+using namespace std;
+int main() {
+   //                                       LAB TASK 01
+    /*int array[10];
+    for (int i = 0; i < 10; i++) {
+        cout << "Enter an integer: ";
+        cin >> array[i];
+    }
+    cout << "The elements in the array are: ";
+    for (int i = 0; i < 10; i++) {
+        cout << array[i] << " ";
+    }
+    return 0;
+    */
+    
+
+
+//                                          LAB TASK 2
+/* int array[5];
+    int sum = 0;
+    int product = 1;
+
+    cout << "Enter 5 integers: "<<endl;
+    for (int i = 0; i < 5; i++) {
+        cin >> array[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+        sum += array[i];
+        product *= array[i];
+    }
+
+    cout << "Sum of elements: " <<sum<<endl;
+    cout << "Product of elements: " <<product<<endl;
+	   return 0 */
+    
+    
+    
+    
+    //                                     LAB TASK 03
+    
+    
+    
+    /*const int size = 5;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = size - 2; i >= 0; --i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+        return 0; }
